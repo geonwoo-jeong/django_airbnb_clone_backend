021ee36304a5ff9a519e5332eeafd80f8f564a1e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-18 15:37:05 +0900

    add custom user model

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ SYSTEM_APPS = [
 
 CUSTOM_APPS = [
     'houses.apps.HousesConfig',
+    'users.apps.UsersConfig',
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth
+
+AUTH_USER_MODEL = 'users.User'
