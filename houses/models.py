188862915c6260404b0b243112e56294aa1e28b5
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-18 17:37:24 +0900

    foreign keys

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -10,6 +11,11 @@ class House(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=140)
     pet_allowed = models.BooleanField()
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
         return self.name
